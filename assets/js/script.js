b462564e4b5f92e95a7147f858e79168eaf96b70
Ravi Kiran <ravi.kiran8844@gmail.com>
2024-02-19 15:32:16 +0530

    updated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,28 +8,22 @@ const preloader = document.getElementById('preloader');
 
 window.addEventListener('load', function () {
   if (preloader) {
-    // // Show the preloader
-    // preloader.style.display = 'flex';
+    // Show the preloader
+    preloader.style.display = 'flex';
 
-    // Set a minimum display time (1 seconds in this example)
-    const minimumDisplayTime = 1000;
+    // Set a minimum display time (1 second in this example)
+    const minimumDisplayTime = 500;
 
     // Get the start time
     const startTime = new Date().getTime();
 
-    // Use setTimeout to hide the preloader after the minimum display time
+    // Calculate the time to wait before hiding the preloader
+    const delayTime = Math.max(0, minimumDisplayTime - (new Date().getTime() - startTime));
+
+    // Hide the preloader after the minimum display time
     setTimeout(function () {
-      const currentTime = new Date().getTime();
-      const elapsedTime = currentTime - startTime;
-
-      // Calculate the remaining time to meet the minimum display time
-      const remainingTime = Math.max(0, minimumDisplayTime - elapsedTime);
-
-      // Use setTimeout again to hide the preloader after the remaining time
-      setTimeout(function () {
-        preloader.style.display = 'none';
-      }, remainingTime);
-    }, 0); // Use 0 for timeout to allow the browser to render the preloader before starting the timeout
+      preloader.style.display = 'none';
+    }, delayTime);
   }
 });
 
